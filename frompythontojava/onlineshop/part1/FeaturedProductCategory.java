c29b462931666cb7818958abb22bb9536b597800
pfolus <imjanczalover18@gmail.com>
2017-08-27 13:26:38 +0200

    Add FeaturedProductCategory class

---
+++ frompythontojava/onlineshop/part1/FeaturedProductCategory.java
@@ -0,0 +1,6 @@
+import java.util.Date;
+
+public class FeaturedProductCategory extends ProductCategory{
+    private Date expirationDate;
+    private Integer ID;
+    private static Integer counter = 1;
