2b74c0efe6d8a3022f5b7cd880706c5292bafaa3
pfolus <imjanczalover18@gmail.com>
2017-08-27 13:26:55 +0200

    Add FeaturedProductCategory class constructor

--- frompythontojava/onlineshop/part1/FeaturedProductCategory.java
+++ frompythontojava/onlineshop/part1/FeaturedProductCategory.java
@@ -4,3 +4,11 @@ public class FeaturedProductCategory extends ProductCategory{
     private Date expirationDate;
     private Integer ID;
     private static Integer counter = 1;
+
+    public FeaturedProductCategory(String name, Date expirationDate){
+        super(name);
+        this.expirationDate = expirationDate;
+        this.ID = counter;
+        counter++;
+    }
+
